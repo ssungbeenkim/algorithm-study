3b18ff2a67ba54744163dd491987c9624f98f0d2
wandakim <nwd1583@naver.com>
2023-02-13 17:05:21 +0900

    2408 modified

--- BOJ/조건문/2480주사위세게.js
+++ BOJ/조건문/2480주사위세게.js
@@ -13,22 +13,43 @@
 
 // 출력
 // 첫째 줄에 게임의 상금을 출력 한다.
-
-const input = `6 2 5`.split(/\s/).map(Number);
-const set = new Set(input);
-const newArr = [...set];
-if (set.size === 1) {
-  console.log(10000 + newArr[0] * 1000);
-} else if (set.size === 2) {
-  for (i = 0; i < newArr.length; i++) {
-    input.splice(input.indexOf(newArr[i]), 1);
+{
+  const input = `6 2 5`.split(/\s/).map(Number);
+  const set = new Set(input);
+  const newArr = [...set];
+  if (set.size === 1) {
+    console.log(10000 + newArr[0] * 1000);
+  } else if (set.size === 2) {
+    for (i = 0; i < newArr.length; i++) {
+      input.splice(input.indexOf(newArr[i]), 1);
+    }
+    const num = input[0];
+    console.log(1000 + num * 100);
+  } else {
+    console.log(Math.max(...input) * 100);
   }
-  const num = input[0];
-  console.log(1000 + num * 100);
-} else {
-  console.log(Math.max(...input) * 100);
 }
-
 // 사실 set을 만들지 않아도 되긴 하고, 경우의 수가 적기 때문에 배열 sort 후 비교하며 if if .. 해도 된다.
 // 근데 뭔가 set 을 활용해서 풀어보고 싶었고, 주사위를 더 여러번 던지더라도 활용할 수 있는 코드로 해보고 싶었다.
 // 근데 그러다가 지쳤다.
+
+// set으로 수정함. forEach사용
+{
+  const fs = require('fs');
+  const input = `${fs.readFileSync('dev/stdin')}`
+    .trim()
+    .split(/\s/)
+    .map(Number);
+  const set = new Set(input);
+  if (set.size === 1) {
+    console.log(10000 + input[0] * 1000);
+  } else if (set.size === 2) {
+    set.forEach((val) => {
+      input.splice(input.indexOf(val), 1);
+    });
+    const num = input[0];
+    console.log(1000 + num * 100);
+  } else {
+    console.log(Math.max(...input) * 100);
+  }
+}
