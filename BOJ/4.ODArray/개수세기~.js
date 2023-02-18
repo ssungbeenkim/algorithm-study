b90e75533179df85562eda820d3b9608088a4db2
wandakim <nwd1583@naver.com>
2023-02-19 00:07:39 +0900

    2562 solved

--- BOJ/4.ODArray/개수세기~.js
+++ BOJ/4.ODArray/개수세기~.js
@@ -30,12 +30,12 @@
     .split(/\s/)
     .map(Number);
 
-  // console.log(
-  //   input
-  //     .slice(2)
-  //     .filter((v) => v < input[1])
-  //     .join(' ')
-  // );
+  console.log(
+    input
+      .slice(2)
+      .filter((v) => v < input[1])
+      .join(' ')
+  );
 }
 
 {
@@ -68,16 +68,35 @@
 
 {
   // 5597 과제 안낸놈 나와
+  // 1. 입력배열. 정렬sort. 1~30 있는 배열에서 빼주고, join
+  // 2. 1~30있는 배열 만들기, 그 배열에서 있는지 확인
+  // 3. 입력된 배열에서 1~30 돌리면서 있는지 확인해서 없는것만 새로운 배열에 넣어주고 join해서 반환하기
+
+  // 3번이 간단할 것 같다.
   const fs = require('fs');
   const inputArray = `${fs.readFileSync('dev/stdin')}`
     .trim()
     .split(/\s/)
     .map(Number);
+
   const unSubmission = [];
+
   for (i = 1; i <= 30; i++) {
     if (!inputArray.includes(i)) {
       unSubmission.push(i);
     }
   }
   console.log(unSubmission.join('\n'));
+}
+
+{
+  // 2562 최대값
+  const fs = require('fs');
+  const inputArr = `${fs.readFileSync('dev/stdin')}`
+    .trim()
+    .split(/\s/)
+    .map(Number);
+  const maximumNum = Math.max(...inputArr);
+  console.log(maximumNum);
+  console.log(inputArr.indexOf(maximumNum) + 1);
 }
