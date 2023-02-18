db95b1b6ec2bc0ad9762a74db04f1242d6fe32f3
wandakim <nwd1583@naver.com>
2023-02-18 19:51:28 +0900

    10871 solved

---
+++ BOJ/4.ODArray/10807개수세기~.js
@@ -0,0 +1,53 @@
+{
+  //10807
+  const fs = require('fs');
+  const input = `${fs.readFileSync('dev/stdin')}`.trim().split('\n');
+
+  // 2번째 줄 인풋을 가지고 반복문을 돌려서 2가 있으면 count up 하고 마지막에 출력하기로 한다.
+
+  const arr = input[1].split(/\s/).map(Number);
+  const num = Number(input[2]);
+
+  function printNumCount(arr, num) {
+    let count = 0;
+    arr.forEach((v) => {
+      if (v === num) {
+        count++;
+      }
+    });
+    console.log(count);
+  }
+
+  printNumCount(arr, num);
+}
+
+//10871
+{
+  const fs = require('fs');
+  const input = `10 5
+1 10 4 9 2 3 8 5 7 6`
+    .trim()
+    .split(/\s/)
+    .map(Number);
+
+  // console.log(
+  //   input
+  //     .slice(2)
+  //     .filter((v) => v < input[1])
+  //     .join(' ')
+  // );
+}
+
+{
+  // 개선 코드 => Array Destructing
+  const fs = require('fs');
+  const input = `10 5
+1 10 4 9 2 3 8 5 7 6`
+    .trim()
+    .split(/\s/)
+    .map(Number);
+
+  const [_, n, ...numbers] = input;
+  const answer = numbers.filter((v) => v < n).join(' ');
+  console.log(answer);
+}
