34e5bfc9e18b47aa6f60365d107190425fcefde7
wandakim <nwd1583@naver.com>
2023-02-23 11:52:22 +0900

    10890 solved

---
+++ BOJ/5.String/10890알파벳찾기.js
@@ -0,0 +1,7 @@
+// // 첫 문자 위치만 찾으면 되므로 indexOf()를 활용.
+// // ascii 번호를 활용해서 알파벳을 불러와보자.  String.fromChatCode(97~122)
+// const fs = require('fs');
+// const inputArr = `baekjoon`.trim().split(/\s/);
+
+const str = 'asdf';
+console.log(str.indexOf(a));
