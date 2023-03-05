af62eb57602e0ec67f2bca88a60864376542d788
wandakim <nwd1583@naver.com>
2023-03-05 23:09:29 +0900

    4344 not solvedd

--- 미제 /4344 평균은 넘겠지.js
+++ 미제 /4344 평균은 넘겠지.js
@@ -1,7 +1,9 @@
 {
+  // 심화 1에 있는 문제.
   // 4344 평균은 넘겠지
-  const fs = require('fs');
-  const inputArr = `${fs.readFileSync('dev/stdin')}`.trim().split(`\n`);
+  const inputArr = `${require('fs').readFileSync('dev/stdin')}`
+    .trim()
+    .split(`\n`);
   const caseArr = inputArr.slice(1);
   caseArr.forEach(printRatio);
 
@@ -20,8 +22,10 @@
   }
 } // 정상적으로 출력이 되는 것 같은데 왜 틀렸다고 하는지 모르겠다.
 {
-  const inputArr = `${fs.readFileSync('dev/stdin')}`.trim().split('\n');
-  const answerArr = [];
+  const inputArr = `${require('fs').readFileSync('dev/stdin')}`
+    .trim()
+    .split('\n');
+
   inputArr.slice(1).forEach(pushAboveRatio);
   console.log(answerArr.join('\n'));
 
@@ -29,7 +33,7 @@
     const scoreArr = caseClass.split(' ').map(Number).slice(1);
     const average = scoreArr.reduce((a, c) => a + c / scoreArr.length, 0);
     const aboveCount = scoreArr.filter((v) => average < v).length;
-    const num = ((aboveCount / caseClass[0]) * 100).toFixed(3);
-    answerArr.push(`${num}%`);
+    const num = ((aboveCount / Number(caseClass[0])) * 100).toFixed(3);
+    console.log(`${num}%`);
   } // 하나씩 출력하는게 문제인가 싶어서 아예 줄바꿈을 해서 출력했는데도 안되고 있음. 질문 남기고 미제로 남기기
 }
