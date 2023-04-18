5e7dc9b356115f042166abc5cc13def289915832
wandakim <nwd1583@naver.com>
2023-04-18 21:10:39 +0900

    24262, 24263 solved

---
+++ BOJ/11.time_complexity/24262~알고리즘수업.js.js
@@ -0,0 +1,12 @@
+{
+  // 24262
+  console.log(1);
+  console.log(0);
+}
+
+{
+  // 24263
+  const input = `${require('fs').readFileSync('dev/stdin')}`.trim();
+  console.log(Number(input));
+  console.log(1);
+}
