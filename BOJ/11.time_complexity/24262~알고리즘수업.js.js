18a2dd95104ffb6c69c3194958029c0179720f01
wandakim <nwd1583@naver.com>
2023-04-19 21:48:26 +0900

    24263 solved

--- BOJ/11.time_complexity/24262~알고리즘수업.js.js
+++ BOJ/11.time_complexity/24262~알고리즘수업.js.js
@@ -1,4 +1,5 @@
 {
+  // 시간복잡도에 관한 문제이다.
   // 24262
   console.log(1);
   console.log(0);
@@ -9,4 +10,15 @@
   const input = `${require('fs').readFileSync('dev/stdin')}`.trim();
   console.log(Number(input));
   console.log(1);
+}
+
+{
+  //24264
+  const input = `${require('fs').readFileSync('dev/stdin')}`.trim();
+  console.log(Number(input) ** 2);
+  console.log(2);
+}
+
+{
+  //24265
 }
