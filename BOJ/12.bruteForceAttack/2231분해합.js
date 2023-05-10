acfd6ebc32c0709e216378253c297dea462fe85f
wandakim <nwd1583@naver.com>
2023-05-10 15:12:05 +0900

    2231 solved

--- BOJ/12.bruteForceAttack/2231분해합.js
+++ BOJ/12.bruteForceAttack/2231분해합.js
@@ -1,7 +1,8 @@
-const n = 216;
+const input = `${require('fs').readFileSync('dev/stdin')}`.trim();
+const n = Number(input);
 
 function findMinimumConstructur(num) {
-  for (i = 1; i < num; i++) {
+  for (i = 1; i <= num; i++) {
     if (isConstructor(i, num)) {
       console.log(i);
       return;
@@ -14,11 +15,14 @@ function isConstructor(inspect, reference) {
   if (inspect < 10) {
     return inspect * 2 === reference ? true : false;
   } else {
-    const sum = inspect
-      .toString()
-      .split('')
-      .map(Number)
-      .reduce(a, (c) => a + c, 0);
+    const sum =
+      inspect
+        .toString()
+        .split('')
+        .map(Number)
+        .reduce((a, c) => a + c, 0) + inspect;
     return sum === reference ? true : false;
   }
 }
+
+findMinimumConstructur(n);
