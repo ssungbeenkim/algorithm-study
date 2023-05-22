50babb4d1e30319edab51e5b38b52924d667bba8
wandakim <nwd1583@naver.com>
2023-05-22 09:05:56 +0900

    1018 solved

--- BOJ/12.bruteForceAttack/1018체스판.js
+++ BOJ/12.bruteForceAttack/1018체스판.js
@@ -1,44 +1,61 @@
 /* 어떻게 풀까
-라인을 만들어서 레퍼런스와 비교해서 다른 갯수를 
+라인을 만들어서 레퍼런스와 비교해서 다른 글자의 갯수를 업데이트 하여 최소값을 찾는다. 
 */
+// input을 라인으로 나누어 배열로
 
-const inputArrLn = `10 13
-BBBBBBBBWBWBW
-BBBBBBBBBWBWB
-BBBBBBBBWBWBW
-BBBBBBBBBWBWB
-BBBBBBBBWBWBW
-BBBBBBBBBWBWB
-BBBBBBBBWBWBW
-BBBBBBBBBWBWB
-WWWWWWWWWWBWB
-WWWWWWWWWWBWB`
+const inputArrLn = `${require('fs').readFileSync('dev/stdin')}`
   .trim()
   .split('\n');
 
 // 행과 열의 사이즈를 x, y 에 할당
-const [x, y] = inputArrLn[0].split(' ').map(Number);
+const [y, x] = inputArrLn[0].split(' ').map(Number);
 
-// 2차원 배열로 보드의 문자를 나눠담는다.
-const matrixArr2d = inputArrLn
-  .slice(1, inputArrLn.length - 1)
-  .map((ln) => ln.split(''));
+// 보드의 문자를 나눠담는다.
+const matrixArr = inputArrLn.slice(1);
 
-// 레퍼런스 배열
+// 레퍼런스 라인 배열
 const refLnArr = [
   'BWBWBWBWWBWBWBWBBWBWBWBWWBWBWBWBBWBWBWBWWBWBWBWBBWBWBWBWWBWBWBWB',
-  'WBWBWBWBBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBW',
+  'WBWBWBWBBWBWBWBWWBWBWBWBBWBWBWBWWBWBWBWBBWBWBWBWWBWBWBWBBWBWBWBW',
 ];
 
-// 임의의 체스보드 라인 생성해서 배열에 때려넣어서 리턴하는 함수 
-function getSampleLns(2dArr) {
-  const Lns = [];
-  for(i=0; i<){
-
+// 모든 경우의 체스보드 8*8=64라인 생성해서 배열에 때려넣기
+function getSampleLns(Arr) {
+  const lns = [];
+  for (i = 0; i < x - 7; i++) {
+    for (j = 0; j < y - 7; j++) {
+      let ln = '';
+      for (k = j; k < j + 8; k++) {
+        ln += Arr[k].slice(i, i + 8);
+      }
+      lns.push(ln);
+    }
   }
+  return lns;
 }
 
+const lns = getSampleLns(matrixArr);
 
-// 레퍼런스 라인과 생성한 라인 비교하며 가장 작게 차이나는 수를 찾는다. 
+// 레퍼런스 라인과 생성한 라인을 하나씩 비교하며 가장 적은 차이를 찾는다.
 
-// 작게 차이나는 수를 64에서 빼면 답이 될 것이다. 
+function difCount(a, b) {
+  let count = 0;
+  for (i = 0; i < 64; i++) {
+    if (a[i] != b[i]) {
+      count++;
+    }
+  }
+  return count;
+}
+
+let minDif = 65;
+refLnArr.forEach((v1) => {
+  lns.forEach((v2) => {
+    const dif = difCount(v1, v2);
+    if (dif < minDif) {
+      minDif = dif;
+    }
+  });
+});
+
+console.log(minDif);
