99a13f435eef3c3a83337df78c7699cdcdb4713c
wandakim <nwd1583@naver.com>
2023-07-01 16:26:11 +0900

    9613 solved

--- BOJ/Algorithm-Basic1/300.Math1/*08.2004조합0의개수.js
+++ BOJ/Algorithm-Basic1/300.Math1/*08.2004조합0의개수.js
@@ -1,33 +1,56 @@
 /* 조합 문자인줄 모르고 뭔가 했다. 
 간단하다 이전 문제와 크게 원리는 다르지 않고 간단한 수학일 뿐이다.  */
 
-const [n, m] = `25 12`.trim().split(/\s/).map(Number);
+{
+  // 시간초과
+  const [n, m] = `25 12`.trim().split(/\s/).map(Number);
 
-const two = xInN(n, 2) - xInN(n - m, 2) - xInN(m, 2);
-const five = xInN(n, 5) - xInN(n - m, 5) - xInN(m, 5);
-if (two === five) {
-  console.log(two);
-} else {
-  console.log(two < five ? two : five);
-}
+  const two = xInN(n, 2) - xInN(n - m, 2) - xInN(m, 2);
+  const five = xInN(n, 5) - xInN(n - m, 5) - xInN(m, 5);
+  if (two === five) {
+    console.log(two);
+  } else {
+    console.log(two < five ? two : five);
+  }
 
-function xInN(n, x) {
-  let count = 0;
-  for (i = 1; i < n + 1; i++) {
-    let cur = i;
-    while (true) {
-      if (cur < x) {
-        break;
-      }
-      if (cur % x === 0) {
-        count++;
-        cur /= x;
-      } else {
-        break;
+  function xInN(n, x) {
+    let count = 0;
+    for (i = 1; i < n + 1; i++) {
+      let cur = i;
+      while (true) {
+        if (cur < x) {
+          break;
+        }
+        if (cur % x === 0) {
+          count++;
+          cur /= x;
+        } else {
+          break;
+        }
       }
     }
+    return count;
   }
-  return count;
+
+  // 아무래도 반복문이 걸린다.
 }
 
-// 소인수의 5가 몇개인지 세면 0의 개수가 된다.
+{
+  const [n, m] = `25 12`.trim().split(/\s/).map(Number);
+
+  const two = xInN(n, 2) - xInN(n - m, 2) - xInN(m, 2);
+  const five = xInN(n, 5) - xInN(n - m, 5) - xInN(m, 5);
+  if (two === five) {
+    console.log(two);
+  } else {
+    console.log(two < five ? two : five);
+  }
+
+  function xInN(n, x) {
+    let count = 0;
+    for (let i = n; x <= i; i /= x) {
+      count += Math.trunc(i / x);
+    }
+    return count;
+  }
+}
