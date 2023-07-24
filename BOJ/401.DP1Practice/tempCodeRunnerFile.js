a9b2349cdef932e5328802dc828d46aea15019c3
wandakim <nwd1583@naver.com>
2023-07-25 06:31:04 +0900

    1390 solved

---
+++ BOJ/401.DP1Practice/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+for (i = 2; i <= n; i++) {
+//   dp[i][x] = dp[i - 1][x] + dp[i - 1][l] + dp[i - 1][r];
+//   dp[i][l] = dp[i - 1][x] + dp[i - 1][r];
+//   dp[i][r] = dp[i - 1][x] + dp[i - 1][l];
+// }
+// console.log(dp);
