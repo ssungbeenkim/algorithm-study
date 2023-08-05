b4d6b8f07f03ea0f40971820cf31c9d438ecbcd0
wandakim <nwd1583@naver.com>
2023-08-05 19:43:50 +0900

    Err: 1476 has an error with node.js

--- BOJ/02.Algorithm-Basic1/402.DP1Challenge/***02.17404RGB거리2.js
+++ BOJ/02.Algorithm-Basic1/402.DP1Challenge/***02.17404RGB거리2.js
@@ -62,4 +62,44 @@
 
 /* 
 다른 풀이를 참고해서 풀어보자. 
+https://bio-info.tistory.com/214 
 */
+{
+  const [count, ...input] = `6
+30 19 5
+64 77 64
+15 19 97
+4 71 57
+90 86 84
+93 32 91`
+    .trim()
+    .split('\n');
+
+  const n = Number(count);
+  const rgb = input.map((v) => v.split(' ').map(Number));
+
+  let ans = Infinity;
+  const I = Infinity;
+
+  for (let i = 0; i < 3; i++) {
+    const dp = Array.from({ length: n }, () => [I, I, I]);
+    dp[0][i] = rgb[0][i];
+
+    for (let j = 1; j < n; j++) {
+      dp[j][0] = rgb[j][0] + Math.min(dp[j - 1][1], dp[j - 1][2]);
+      dp[j][1] = rgb[j][1] + Math.min(dp[j - 1][0], dp[j - 1][2]);
+      dp[j][2] = rgb[j][2] + Math.min(dp[j - 1][0], dp[j - 1][1]);
+    }
+
+    for (let k = 0; k < 3; k++) {
+      if (i !== k) {
+        ans = Math.min(ans, dp[n - 1][k]);
+      }
+    }
+  }
+
+  console.log(ans);
+}
+
+/* 초기값을 Infinity로 해주어 첫번째 집이 각 숫자로 선택되게 한다는 아이디어를 
+직관적으로 받아들이기 어려웠다. 나중에 다시 풀어보기.  */
