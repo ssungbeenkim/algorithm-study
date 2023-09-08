a8e4e4719c5d1f4f946ff1d76c3fdd62266158c7
wandakim <nwd1583@naver.com>
2023-09-08 17:52:15 +0900

    permutaion

--- study/methodology/permutation.js
+++ study/methodology/permutation.js
@@ -1,20 +1,42 @@
-const getPermutations = function (arr, selectNumber) {
-  const results = [];
-  if (selectNumber === 1) return arr.map((el) => [el]);
-  // n개중에서 1개 선택할 때(nP1), 바로 모든 배열의 원소 return. 1개선택이므로 순서가 의미없음.
+{
+  const getPermutations = function (arr, selectNumber) {
+    const results = [];
+    if (selectNumber === 1) return arr.map((el) => [el]);
+    // n개중에서 1개 선택할 때(nP1), 바로 모든 배열의 원소 return. 1개선택이므로 순서가 의미없음.
 
-  arr.forEach((fixed, index, origin) => {
-    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
-    // 해당하는 fixed를 제외한 나머지 배열
-    const permutations = getPermutations(rest, selectNumber - 1);
-    // 나머지에 대해서 순열을 구한다.
-    const attached = permutations.map((el) => [fixed, ...el]);
-    //  돌아온 순열에 떼 놓은(fixed) 값 붙이기
-    results.push(...attached);
-    // 배열 spread syntax 로 모두다 push
-  });
+    arr.forEach((fixed, index, origin) => {
+      const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+      // 해당하는 fixed를 제외한 나머지 배열
+      const permutations = getPermutations(rest, selectNumber - 1);
+      // 나머지에 대해서 순열을 구한다.
+      const attached = permutations.map((el) => [fixed, ...el]);
+      //  돌아온 순열에 떼 놓은(fixed) 값 붙이기
+      results.push(...attached);
+      // 배열 spread syntax 로 모두다 push
+    });
 
-  return results; // 결과 담긴 results return
-};
+    return results; // 결과 담긴 results return
+  };
 
-console.log(getPermutations([1, 2, 3, 4], 3));
+  console.log(getPermutations([1, 2, 3, 4], 3));
+}
+
+{
+  // generator 활용
+  const permutations = function* (elements, selectNumber) {
+    for (let i = 0; i < elements.length; i++) {
+      if (selectNumber === 1) {
+        yield [elements[i]];
+      } else {
+        const fixed = elements[i];
+        const rest = [...elements.slice(0, i), ...elements.slice(i + 1)];
+        for (const a of permutations(rest, selectNumber - 1)) {
+          yield [fixed, ...a];
+        }
+      }
+    }
+  };
+
+  const combi = permutations(['a', 'b', 'c', 'd'], 3);
+  console.log(...combi);
+}
